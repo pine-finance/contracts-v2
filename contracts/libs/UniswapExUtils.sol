bb3cd0a2fc5fd0a253aa2af95a707a6bc55626bf
Ignacio Mazzara <nachomazzara@gmail.com>
2020-08-22 11:41:11 -0300

    chore: utils dev notation

--- contracts/libs/UniswapExUtils.sol
+++ contracts/libs/UniswapExUtils.sol
@@ -9,6 +9,12 @@ import "../libs/SafeERC20.sol";
 library UniswapexUtils {
     address internal constant ETH_ADDRESS = address(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
 
+    /**
+     * @notice Get the account's balance of token or ETH
+     * @param _token - Address of the token
+     * @param _addr - Address of the account
+     * @return uint256 - Account's balance of token or ETH
+     */
     function balanceOf(IERC20 _token, address _addr) internal view returns (uint256) {
         if (ETH_ADDRESS == address(_token)) {
             return _addr.balance;
@@ -17,6 +23,13 @@ library UniswapexUtils {
         return _token.balanceOf(_addr);
     }
 
+     /**
+     * @notice Transfer token or ETH to a destinatary
+     * @param _token - Address of the token
+     * @param _to - Address of the recipient
+     * @param _val - Uint256 of the amount to transfer
+     * @return bool - Whether the transfer was success or not
+     */
     function transfer(IERC20 _token, address _to, uint256 _val) internal returns (bool) {
         if (ETH_ADDRESS == address(_token)) {
             (bool success, ) = _to.call{value:_val}("");
