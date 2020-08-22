19a86e0245b6b7bcffd55e0c427bf696851f5f78
Ignacio Mazzara <nachomazzara@gmail.com>
2020-08-22 09:55:44 -0300

    chore: limit orders module dev notation

--- contracts/interfaces/IModule.sol
+++ contracts/interfaces/IModule.sol
@@ -3,9 +3,21 @@ pragma solidity ^0.6.8;
 
 import "../interfaces/IERC20.sol";
 
+
+
 interface IModule {
+    /// @notice receive ETH
     receive() external payable;
 
+    /**
+     * @notice Executes an order
+     * @param _inputToken - Address of the input token
+     * @param _inputAmount - uint256 of the input token amount (order amount)
+     * @param _owner - Address of the order's owner
+     * @param _data - Bytes of the order's data
+     * @param _auxData - Bytes of the auxiliar data used for the handlers to execute the order
+     * @return bought - amount of output token bought
+     */
     function execute(
         IERC20 _inputToken,
         uint256 _inputAmount,
@@ -14,6 +26,14 @@ interface IModule {
         bytes calldata _auxData
     ) external returns (uint256 bought);
 
+    /**
+     * @notice Check whether an order can be executed or not
+     * @param _inputToken - Address of the input token
+     * @param _inputAmount - uint256 of the input token amount (order amount)
+     * @param _data - Bytes of the order's data
+     * @param _auxData - Bytes of the auxiliar data used for the handlers to execute the order
+     * @return bool - whether the order can be executed or not
+     */
     function canExecute(
         IERC20 _inputToken,
         uint256 _inputAmount,
--- contracts/modules/LimitOrder.sol
+++ contracts/modules/LimitOrder.sol
@@ -8,11 +8,22 @@ import "../commons/Order.sol";
 import "../libs/SafeMath.sol";
 
 
+/// @notice Module used to execute limit orders create in the core contract
 contract LimitOrder is IModule, Order {
     using SafeMath for uint256;
 
+    /// @notice receive ETH
     receive() external override payable { }
 
+    /**
+     * @notice Executes an order
+     * @param _inputToken - Address of the input token
+     * @param _inputAmount - uint256 of the input token amount (order amount)
+     * @param _owner - Address of the order's owner
+     * @param _data - Bytes of the order's data
+     * @param _auxData - Bytes of the auxiliar data used for the handlers to execute the order
+     * @return bought - amount of output token bought
+     */
     function execute(
         IERC20 _inputToken,
         uint256 _inputAmount,
@@ -51,6 +62,14 @@ contract LimitOrder is IModule, Order {
         return bought;
     }
 
+    /**
+     * @notice Check whether an order can be executed or not
+     * @param _inputToken - Address of the input token
+     * @param _inputAmount - uint256 of the input token amount (order amount)
+     * @param _data - Bytes of the order's data
+     * @param _auxData - Bytes of the auxiliar data used for the handlers to execute the order
+     * @return bool - whether the order can be executed or not
+     */
     function canExecute(
         IERC20 _inputToken,
         uint256 _inputAmount,
@@ -78,6 +97,11 @@ contract LimitOrder is IModule, Order {
         );
     }
 
+    /**
+     * @notice Get this contract's balance of token or Ether
+     * @param _token - Address of the input token
+     * @return uint256 - this contract's balance of _token
+     */
     function _getBalance(IERC20 _token) internal view returns (uint256) {
         if (address(_token) == ETH_ADDRESS) {
             return address(this).balance;
@@ -86,6 +110,12 @@ contract LimitOrder is IModule, Order {
         }
     }
 
+    /**
+     * @notice Transfer token or Ether amount to a recipient
+     * @param _token - Address of the token
+     * @param _to - Address of the recipient
+     * @param _amount - uint256 of the amount to be transferred
+     */
     function _transferAmount(
         IERC20 _token,
         address payable _to,
