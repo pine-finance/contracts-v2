1d5e86034020605ad0ed24d9fe59faac5b8fc72f
Ignacio Mazzara <nachomazzara@gmail.com>
2020-09-11 13:27:01 -0300

    feat: 1nch handler

--- full/OneInchHandler.sol
+++ full/OneInchHandler.sol
@@ -155,7 +155,6 @@ library SafeMath {
 
 // File: contracts/interfaces/IERC20.sol
 
-// SPDX-License-Identifier: GPL-2.0
 
 pragma solidity ^0.6.8;
 
@@ -237,7 +236,6 @@ interface IERC20 {
 
 // File: contracts/libs/SafeERC20.sol
 
-// SPDX-License-Identifier: GPL-2.0
 
 pragma solidity ^0.6.8;
 
@@ -252,7 +250,6 @@ library SafeERC20 {
 
 // File: contracts/libs/PineUtils.sol
 
-// SPDX-License-Identifier: GPL-2.0
 
 pragma solidity ^0.6.8;
 
@@ -295,7 +292,6 @@ library PineUtils {
 
 // File: contracts/commons/Order.sol
 
-// SPDX-License-Identifier: GPL-2.0
 
 pragma solidity ^0.6.8;
 
@@ -304,22 +300,8 @@ contract Order {
     address public constant ETH_ADDRESS = address(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
 }
 
-// File: contracts/interfaces/IWETH.sol
-
-// SPDX-License-Identifier: GPL-2.0
-
-pragma solidity ^0.6.8;
-
-
-
-interface IWETH is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint wad) external;
-}
-
 // File: contracts/interfaces/IHandler.sol
 
-// SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.6.8;
 
 
@@ -362,9 +344,8 @@ interface IHandler {
     ) external view returns (bool);
 }
 
-// File: contracts/handlers/BalancerHandler.sol
+// File: contracts/handlers/OneInchHandler.sol
 
-// SPDX-License-Identifier: GPL-2.0
 
 pragma solidity ^0.6.8;
 
@@ -373,26 +354,32 @@ pragma solidity ^0.6.8;
 
 
 
-
 /// @notice UniswapV1 Handler used to execute an order
 
-interface PoolInterface {
-    function swapExactAmountIn(address, uint, address, uint, uint) external returns (uint, uint);
+interface IOneSplitWrapper {
+    function swap(
+        address fromToken,
+        address toToken,
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] calldata distribution,
+        uint256 flags
+    ) external payable;
 }
 
-contract BalancerHandler is IHandler, Order {
+contract OneinchHandler is IHandler, Order {
     using SafeMath for uint256;
 
-    IWETH public immutable WETH;
+    IOneSplitWrapper public immutable oneSplitWrapper;
 
     uint256 private constant never = uint(-1);
 
     /**
      * @notice Creates the handler
-     * @param _weth - Address of WETH contract
+     * @param _oneSplitWrapper - Address of oneSplitWrapper
      */
-    constructor(IWETH _weth) public {
-        WETH = _weth;
+    constructor(IOneSplitWrapper _oneSplitWrapper) public {
+        oneSplitWrapper = _oneSplitWrapper;
     }
 
     /// @notice receive ETH
@@ -415,57 +402,36 @@ contract BalancerHandler is IHandler, Order {
         bytes calldata _data
     ) external payable override returns (uint256 bought) {
         // Load real initial balance, don't trust provided value
-        uint256 amount = PineUtils.balanceOf(_inputToken, address(this));
+        uint256 inputAmount = PineUtils.balanceOf(_inputToken, address(this));
         address inputToken = address(_inputToken);
         address outputToken = address(_outputToken);
-        address weth = address(WETH);
 
-        (,address payable relayer, uint256 fee, address payable poolA, address payable poolB) =
-            abi.decode(_data, (address, address, uint256, address, address));
+        (,address payable relayer, uint256 fee, uint256 flag, uint256[] memory distributionsA, uint256[] memory distributionsB) =
+             abi.decode(_data, (address, address, uint256, uint256, uint256[], uint256[]));
 
-        if (inputToken == weth || inputToken == PineUtils.ETH_ADDRESS) {
-            // Swap WETH -> outputToken
-            amount = amount.sub(fee);
-
-            // Convert from ETH to WETH if necessary
-            if (inputToken == PineUtils.ETH_ADDRESS) {
-                WETH.deposit{ value: amount }();
-                inputToken = weth;
-            } else {
-                WETH.withdraw(fee);
-            }
-
-            // Trade
-            bought = _swap(poolA, inputToken, outputToken, amount, msg.sender);
-        } else if (outputToken == weth || outputToken == PineUtils.ETH_ADDRESS) {
-            // Swap inputToken -> WETH
-            bought = _swap(poolA, inputToken, weth, amount, address(this));
-
-            // Convert from WETH to ETH if necessary
-            if (outputToken == PineUtils.ETH_ADDRESS) {
-                WETH.withdraw(bought);
-            } else {
-                WETH.withdraw(fee);
-            }
-
-            // Transfer amount to sender
+        if (inputToken == ETH_ADDRESS) {
+            // Keep some eth for paying the fee
+            uint256 sell = inputAmount.sub(fee);
+            bought = _swap(inputToken, outputToken, sell, msg.sender, distributionsA, flag);
+        } else if (outputToken == ETH_ADDRESS) {
+            // Convert
+            bought = _swap(inputToken, outputToken, inputAmount, address(this), distributionsA, flag);
             bought = bought.sub(fee);
-            PineUtils.transfer(IERC20(outputToken), msg.sender, bought);
+
+            // Send amount bought
+            (bool successSender,) = msg.sender.call{value: bought}("");
+            require(successSender, "UniswapV1Handler#handle: TRANSFER_ETH_TO_CALLER_FAILED");
         } else {
-            // Swap inputToken -> WETH -> outputToken
-            //  - inputToken -> WETH
-            bought = _swap(poolA, inputToken, weth, amount, address(this));
+            // Convert from fromToken to ETH
+            uint256 boughtEth = _swap(inputToken, ETH_ADDRESS, inputAmount, address(this), distributionsA, flag);
 
-            // Withdraw fee
-            WETH.withdraw(fee);
-
-            // - WETH -> outputToken
-            bought = _swap(poolB, weth, outputToken, bought.sub(fee), msg.sender);
+            // Convert from ETH to toToken
+            bought = _swap(ETH_ADDRESS, outputToken, boughtEth.sub(fee), msg.sender, distributionsB, flag);
         }
 
         // Send fee to relayer
         (bool successRelayer,) = relayer.call{value: fee}("");
-        require(successRelayer, "UniswapV2Handler#handle: TRANSFER_ETH_TO_RELAYER_FAILED");
+        require(successRelayer, "UniswapV1Handler#handle: TRANSFER_ETH_TO_RELAYER_FAILED");
     }
 
     /**
@@ -488,34 +454,45 @@ contract BalancerHandler is IHandler, Order {
      * @param _outputToken - Address of the output token
      * @param _inputAmount - uint256 of the input token amount
      * @param _recipient - Address of the recipient
+     * @param _distributions - Array of weights for volume distribution
+     * @param _flag - flags
      * @return bought - Amount of output token bought
      */
     function _swap(
-        address _pool,
         address _inputToken,
         address _outputToken,
         uint256 _inputAmount,
-        address _recipient
+        address _recipient,
+        uint256[] memory _distributions,
+        uint256 _flag
     ) internal returns (uint256 bought) {
         // Check if previous allowance is enough and approve the pool if not
         IERC20 inputToken = IERC20(_inputToken);
+        uint256 value = 0;
 
-        uint256 prevAllowance = inputToken.allowance(address(this), _pool);
-        if (prevAllowance < _inputAmount) {
-            if (prevAllowance != 0) {
-                inputToken.approve(_pool, 0);
+        if (_inputToken == ETH_ADDRESS) {
+            value = _inputAmount;
+        } else {
+            uint256 prevAllowance = inputToken.allowance(address(this), address(oneSplitWrapper));
+            if (prevAllowance < _inputAmount) {
+                if (prevAllowance != 0) {
+                    inputToken.approve(address(oneSplitWrapper), 0);
+                }
+
+                inputToken.approve(address(oneSplitWrapper), uint(-1));
             }
-
-            inputToken.approve(_pool, uint(-1));
         }
 
-        (bought,) = PoolInterface(_pool).swapExactAmountIn(
+        oneSplitWrapper.swap{ value: value }(
             _inputToken,
-            _inputAmount,
             _outputToken,
+            _inputAmount,
             0,
-            never
+            _distributions,
+            _flag
         );
+
+        bought = PineUtils.balanceOf(IERC20(_outputToken), address(this));
 
         if (_recipient != address(this)) {
             PineUtils.transfer(IERC20(_outputToken), _recipient, bought);
--- scripts/buildFull.sh
+++ scripts/buildFull.sh
@@ -5,6 +5,7 @@ LIMIT_ORDERS_MODULE=LimitOrders.sol
 UNISWAP_V1_HANDLER=UniswapV1Handler.sol
 UNISWAP_V2_HANDLER=UniswapV2Handler.sol
 BALANCER_HANDLER=BalancerHandler.sol
+ONEINCH_HANDLER=OneInchHandler.sol
 
 OUTPUT=full
 
@@ -13,3 +14,4 @@ npx truffle-flattener contracts/modules/$LIMIT_ORDERS_MODULE > $OUTPUT/$LIMIT_OR
 npx truffle-flattener contracts/handlers/$UNISWAP_V1_HANDLER > $OUTPUT/$UNISWAP_V1_HANDLER
 npx truffle-flattener contracts/handlers/$UNISWAP_V2_HANDLER > $OUTPUT/$UNISWAP_V2_HANDLER
 npx truffle-flattener contracts/handlers/$BALANCER_HANDLER > $OUTPUT/$BALANCER_HANDLER
+npx truffle-flattener contracts/handlers/$ONEINCH_HANDLER > $OUTPUT/$ONEINCH_HANDLER
