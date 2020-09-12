0b3743d607961f2d2503eea93bd59bba6709a229
Agustin Aguilar <Agusxrun@gmail.com>
2020-09-12 19:27:37 +0200

    Add splitter contract

--- test/helpers/balanceSnap.ts
+++ test/helpers/balanceSnap.ts
@@ -7,6 +7,10 @@ const expect = require('chai')
   .use(require('bn-chai')(BN))
   .expect
 
+export function bn(n: any) {
+  return new BN(n)
+}
+
 export async function balanceSnap(token: any, address: string, account: string = "") {
   let snapBalance = await token.balanceOf(address)
   return {
@@ -20,9 +24,9 @@ export async function balanceSnap(token: any, address: string, account: string =
     },
     requireIncrease: async function (delta) {
       const realincrease = (await token.balanceOf(address)).sub(snapBalance)
-      const expectedBalance = snapBalance.add(delta)
+      const expectedBalance = snapBalance.add(bn(delta))
       expect(
-        snapBalance.add(delta),
+        snapBalance.add(bn(delta)),
         `${account} should increase by ${delta} - but increased by ${realincrease}`
       ).to.eq.BN(
         await token.balanceOf(address)
@@ -32,9 +36,9 @@ export async function balanceSnap(token: any, address: string, account: string =
     },
     requireDecrease: async function (delta) {
       const realdecrease = snapBalance.sub(await token.balanceOf(address))
-      const expectedBalance = snapBalance.sub(delta)
+      const expectedBalance = snapBalance.sub(bn(delta))
       expect(
-        snapBalance.sub(delta),
+        snapBalance.sub(b(delta)),
         `${account} should decrease by ${delta} - but decreased by ${realdecrease}`
       ).to.eq.BN(
         await token.balanceOf(address)
@@ -61,7 +65,7 @@ export async function etherSnap(address: string, account: string = "") {
     },
     requireIncrease: async function (delta) {
       const realincrease = new BN(await web3.eth.getBalance(address)).sub(snapBalance)
-      const expectedBalance = snapBalance.add(delta)
+      const expectedBalance = snapBalance.add(bn(delta))
       expect(
         expectedBalance,
         `${account} should increase by ${delta} - but increased by ${realincrease}`
@@ -73,9 +77,9 @@ export async function etherSnap(address: string, account: string = "") {
     },
     requireDecrease: async function (delta) {
       const realdecrease = snapBalance.sub(new BN(await web3.eth.getBalance(address)))
-      const expectedBalance = snapBalance.sub(delta)
+      const expectedBalance = snapBalance.sub(bn(delta))
       expect(
-        snapBalance.sub(delta),
+        snapBalance.sub(b(delta)),
         `${account} should decrease by ${delta} - but decreased by ${realdecrease}`
       ).to.eq.BN(
         new BN(await web3.eth.getBalance(address))
